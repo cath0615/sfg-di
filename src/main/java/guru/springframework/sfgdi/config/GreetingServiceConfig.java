86076210682cbcab36d60259cfd1b364e5925bf5
cath0615 <62917107+cath0615@users.noreply.github.com>
2021-07-17 15:29:47 +0800

    Update GreetingServiceConfig.java

--- src/main/java/guru/springframework/sfgdi/config/GreetingServiceConfig.java
+++ src/main/java/guru/springframework/sfgdi/config/GreetingServiceConfig.java
@@ -10,9 +10,15 @@ import org.springframework.context.annotation.Configuration;
  * Created by jt on 2/20/21.
  */
 @Configuration
+//after spring boot does a component scan to find this configuration class, @Configuration
+//tells spring this is a configuration class and defines a different bean.
 public class GreetingServiceConfig {
 
+    //make it a spring managed component, we ususally do this with third party library
+    //instead of doing it on class that we own(we use stereotype annotation instead)
     @Bean
+    //we delete the @service in constructorGreetingService class and we use this method 
+    //to execute to build it.
     ConstructorGreetingService constructorGreetingService(){
         return new ConstructorGreetingService();
     }
